42da2de8e25055765de44accbc442df943ab9f39
unknown <franklinluiz2000@gmail.com>
2025-07-27 10:33:37 -0300

    implements userservice and password encrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
 })
 export class AppModule {}
